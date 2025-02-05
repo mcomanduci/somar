ee394c98b851983a2bf9519a35509057a0ef952a
Marcelo Comanduci <mcomanduci@gmail.com>
2025-02-05 19:17:39 -0300

    services

--- script/script.js
+++ script/script.js
@@ -1,17 +1,9 @@
-// const flipCard = document.querySelector(".more");
-// const flipCard2 = document.querySelector(".morez");
-// const innerCard = document.querySelector("article.bg-white");
-// const teste = document.querySelector(".teste")
+const flipCards = document.querySelectorAll(".more");
 
-// function flip() {
-//   innerCard.classList.toggle("change");
-//   teste.classList.toggle("absolute")
-// }
-// flipCard.addEventListener("click", flip);
-// flipCard2.addEventListener("click", flip);
-
-document.querySelectorAll('.more').forEach(button => {
-  button.addEventListener('click', () => {
-    document.querySelector('.inner').classList.toggle('[transform:rotateY(180deg)]');
+flipCards.forEach((button) => {
+  button.addEventListener("click", () => {
+    const innerElement = button.closest(".inner");
+    innerElement &&
+      innerElement.classList.toggle("[transform:rotateY(180deg)]");
   });
 });

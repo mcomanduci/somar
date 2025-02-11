2304860e8a42f4b880dd902716a36674ae1cd2c8
Marcelo Comanduci <mcomanduci@gmail.com>
2025-02-11 19:46:12 -0300

    intermediario

--- script/script.js
+++ script/script.js
@@ -1,3 +1,7 @@
+import keenSliderInit from "./modules/keen-slider-init.js";
+
+keenSliderInit();
+
 const flipCards = document.querySelectorAll(".more");
 
 flipCards.forEach((button) => {
@@ -14,31 +18,4 @@ flipCards.forEach((button) => {
     innerElement &&
       innerElement.classList.toggle("[transform:rotateY(180deg)]");
   });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  // Toggle menu mobile
-  const menuButton = document.querySelector('button[aria-label="Menu"]');
-  const mobileMenu = document.querySelector("#mobile-menu");
-
-  menuButton?.addEventListener("click", () => {
-    const expanded =
-      menuButton.getAttribute("aria-expanded") === "true" || false;
-    menuButton.setAttribute("aria-expanded", !expanded);
-    mobileMenu.classList.toggle("hidden");
-  });
-
-  // Header scroll effect
-  const header = document.querySelector("header");
-  window.addEventListener(
-    "scroll",
-    () => {
-      if (window.scrollY > 0) {
-        header.classList.add("shadow-md", "bg-white/50", "backdrop-blur-sm");
-      } else {
-        header.classList.remove("shadow-md", "bg-white/50", "backdrop-blur-sm");
-      }
-    },
-    { passive: true }
-  );
 });

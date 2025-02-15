547a7a4150b2b32c7da6de340ab44024716fec3c
Marcelo Comanduci <mcomanduci@gmail.com>
2025-02-15 10:50:43 -0300

    first commit after

--- script/script.js
+++ script/script.js
@@ -1,21 +1,15 @@
 import keenSliderInit from "./modules/keen-slider-init.js";
 
-keenSliderInit();
+document.addEventListener("DOMContentLoaded", function () {
+  keenSliderInit();
 
-const flipCards = document.querySelectorAll(".more");
+  const flipCards = document.querySelectorAll(".more");
 
-flipCards.forEach((button) => {
-  button.addEventListener("click", () => {
-    const innerElement = button.closest(".service-card-inner");
-    innerElement &&
-      innerElement.classList.toggle("[transform:rotateY(180deg)]");
-  });
-});
-
-flipCards.forEach((button) => {
-  button.addEventListener("click", () => {
-    const innerElement = button.closest(".inner");
-    innerElement &&
-      innerElement.classList.toggle("[transform:rotateY(180deg)]");
+  flipCards.forEach((button) => {
+    button.addEventListener("click", () => {
+      const innerElement = button.closest(".service-card-inner");
+      innerElement &&
+        innerElement.classList.toggle("[transform:rotateY(180deg)]");
+    });
   });
 });
